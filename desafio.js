9e557562f690f377a3b6091922b5035dd2fd2079
Gutemberg <gutemberg.jhonata@gmail.com>
2022-07-29 21:05:05 -0300

    Refatorando projeto com ESModules

--- desafio.js
+++ desafio.js
@@ -1,9 +1,9 @@
-const readline = require("readline-sync")
+import { question } from 'readline-sync'
 
 let score = 0
 
 while (true) {
-    const level = readline.question('Escolha o nivel (1, 2 ou 3): ')
+    const level = question('Escolha o nivel (1, 2 ou 3): ')
 
     if (level == 0) {
         break
@@ -15,17 +15,17 @@ while (true) {
 
     switch (level) {
         case "1":
-            number = readline.question('Digite um numero de 0 a 1: ')
+            number = question('Digite um numero de 0 a 1: ')
             random = Math.floor(Math.random())
             scorePoints = 5
             break
         case "2":
-            number = readline.question('Digite um numero de 0 a 5: ')
+            number = question('Digite um numero de 0 a 5: ')
             random = Math.floor(5 * Math.random())
             scorePoints = 10
             break
         case "3":
-            number = readline.question('Digite um numero de 0 a 10: ')
+            number = question('Digite um numero de 0 a 10: ')
             random = Math.floor(10 * Math.random())
             scorePoints = 15
             break
