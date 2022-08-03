cfa4663d9a27d2ed6c49702bd7bde3c5dd9a7eed
Gutemberg <gutemberg.jhonata@gmail.com>
2022-08-03 20:03:53 -0300

    Refatorando código com questionInt

--- desafio.js
+++ desafio.js
@@ -1,11 +1,11 @@
-import { question } from 'readline-sync'
+import { questionInt } from 'readline-sync'
 
 let score = 0
 
 while (true) {
-    const level = question('Escolha o nivel (1, 2 ou 3): ')
+    const level = questionInt('Escolha o nivel (1, 2 ou 3): ')
 
-    if (level == 0) {
+    if (level === 0) {
         break
     }
 
@@ -14,30 +14,32 @@ while (true) {
     let scorePoints
 
     switch (level) {
-        case "1":
-            number = question('Digite um numero de 0 a 1: ')
+        case 1:
+            number = questionInt('Digite um numero de 0 a 1: ')
             random = Math.floor(Math.random())
             scorePoints = 5
             break
-        case "2":
-            number = question('Digite um numero de 0 a 5: ')
+        case 2:
+            number = questionInt('Digite um numero de 0 a 5: ')
             random = Math.floor(5 * Math.random())
             scorePoints = 10
             break
-        case "3":
-            number = question('Digite um numero de 0 a 10: ')
+        case 3:
+            number = questionInt('Digite um numero de 0 a 10: ')
             random = Math.floor(10 * Math.random())
             scorePoints = 15
             break
     }
 
-    if (number == random) { 
-        console.log("Ganhou!")
-        score += scorePoints 
-    } else {
-        console.log("Perdeu!")
+    if (random != undefined) {
+        if (number === random) { 
+            console.log("Ganhou!")
+            score += scorePoints 
+        } else {
+            console.log("Perdeu!")
+        }
+    
+        console.log(`Número gerado: ${random}`)
+        console.log(`Sua pontuação: ${score}`)
     }
-
-    console.log(`Número gerado: ${random}`)
-    console.log(`Sua pontuação: ${score}`)
 }
