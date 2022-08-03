1d41488d9093291962eb98426fd48ef5c44e27a2
Gutemberg <gutemberg.jhonata@gmail.com>
2022-08-03 20:04:24 -0300

    Adicionando conceito operadorIgualdade

---
+++ conceitos/operadorIgualdade.js
@@ -0,0 +1,14 @@
+// = (atribuição)
+// == (igualdade)
+// === (igualdade com comparação de tipos)
+// dinâmicamente tipada
+// inferência de tipos
+
+const numero1 = 1;
+const numero2 = "1";
+
+if (numero1 === numero2) {
+    console.log(true)
+} else {
+    console.log(false)
+}
